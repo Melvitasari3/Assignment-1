d4afe79c8fb747aefadfcc09e3765755b429c072
Melvitasari3 <melvitasari316@gmail.com>
2021-11-22 17:06:34 +0700

    first commitassignment-1

--- biodata.go
+++ biodata.go
@@ -6,8 +6,8 @@ import (
 	"strconv"
 )
 
-type biodata struct {
-	absen     int
+type namaSiswa struct {
+	absensi   int
 	nama      string
 	alamat    string
 	pekerjaan string
@@ -16,35 +16,28 @@ type biodata struct {
 
 func main() {
 	Program := os.Args
-	index, _ := strconv.Atoi(Program[1])
-
-	var student = []biodata{
-		{absen: 1, nama: "Melvita", alamat: "Dusun 1 Desa Lubuk Saban Kecamatan Pantai Cermin Kabupaten Serdang Bedagai Provinsi Sumatera Utara", pekerjaan: "Mahasiswa", alasan: "Ingin belajar pemograman golang dan mendapatkan lebih banyak teman"},
-		{absen: 2, nama: "Yola", alamat: "Dusun 5 Desa wonosari Kecamatan Pantai Cermin Kabupaten Serdang Bedagai Provinsi Sumatera Utara", pekerjaan: "Mahasiswa", alasan: "Ingin belajar pemograman golang "},
-		{absen: 3, nama: "Rahma", alamat: "Dusun 1 Desa jagung Kecamatan Pantai Cermin Kabupaten Serdang Bedagai Provinsi Sumatera Utara", pekerjaan: "Mahasiswa", alasan: "Ingin mendapatkan lebih banyak teman"},
-	}
-	getStudent(student, index)
+	idSiswa, _ := strconv.Atoi(Program[1])
+	getDataKelas(idSiswa)
+	// fmt.Println(namaProgram)
 }
 
-func getStudent(student []biodata, absen int) {
-	var index int
-	for i := 0; i < len(student); i++ {
-		if student[i].absen == absen {
-			index = i
-			goto suc
-		} else if i == (len(student) - 1) {
-			goto empty
-		}
+var dataKelas []namaSiswa
+
+func getDataKelas(noabsen int) {
+	dataKelas = []namaSiswa{
+		{absensi: 1, nama: "Melvita", alamat: "Dusun 1 Desa Lubuk Saban Kecamatan Pantai Cermin Kabupaten Serdang Bedagai Provinsi Sumatera Utara", pekerjaan: "Mahasiswa", alasan: "Ingin belajar pemograman golang dan mendapatkan lebih banyak teman"},
+		{absensi: 2, nama: "Yola", alamat: "Dusun 5 Desa wonosari Kecamatan Pantai Cermin Kabupaten Serdang Bedagai Provinsi Sumatera Utara", pekerjaan: "Mahasiswa", alasan: "Ingin belajar pemograman golang "},
+		{absensi: 3, nama: "Rahma", alamat: "Dusun 1 Desa jagung Kecamatan Pantai Cermin Kabupaten Serdang Bedagai Provinsi Sumatera Utara", pekerjaan: "Mahasiswa", alasan: "Ingin mendapatkan lebih banyak teman"},
 	}
 
-suc:
-	fmt.Printf("Nama : %v\n", student[index].nama)
-	fmt.Printf("Alamat : %v\n", student[index].alamat)
-	fmt.Printf("Pekerjaan : %v\n", student[index].pekerjaan)
-	fmt.Printf("Alasan memilih kelas Golang : %v\n", student[index].alasan)
-	return
+	for _, siswa := range dataKelas {
 
-empty:
-	fmt.Println("Nomor absen yang anda cari tidak terdaftar")
-	return
+		if siswa.absensi == noabsen {
+			fmt.Println("Nama : ", siswa.nama)
+			fmt.Println("Alamat : ", siswa.alamat)
+			fmt.Println("Pekerjaan : ", siswa.pekerjaan)
+			fmt.Println("Alasan memilih kelas Golang : ", siswa.alasan)
+
+		}
+	}
 }
